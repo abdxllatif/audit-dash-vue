de7c2ec610912e1724c0841c442360abe9dcb979
Abdellatif Beldjelti <a.beldjelti@esi-sba.dz>
2021-05-09 18:45:08 +0100

    Add "new" departement and formation views

---
+++ src/views/insertion/NewDep.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <title-bar :title-stack="titleStack"/>
+    <hero-bar>
+      Insertion
+      <router-link slot="right" to="/dash" class="button">
+        Dashboard
+      </router-link>
+    </hero-bar>
+    <section class="section is-main-section">
+      <card-component title="Nouveau département" icon="ballot">
+        <form @submit.prevent="submit">
+          <b-field label="Nom du département" horizontal>
+            <b-field>
+              <b-input icon="account" v-model="form.name" placeholder="Nom du département" name="name" required />
+            </b-field>
+          </b-field>
+          <b-field label="Description" message="Ne pas dépasser 255 caractères" horizontal>
+            <b-input type="textarea" placeholder="Une petite définition du département" v-model="form.description" maxlength="255" required/>
+          </b-field>
+          <b-field horizontal>
+            <b-field grouped>
+              <div class="control">
+                <b-button native-type="submit" type="is-primary">Ajouter</b-button>
+              </div>
+            </b-field>
+          </b-field>
+        </form>
+      </card-component>
+    </section>
+  </div>
+</template>
+
+<script>
+import TitleBar from '@/components/TitleBar'
+import CardComponent from '@/components/CardComponent'
+import HeroBar from '@/components/HeroBar'
+
+export default {
+  name: 'newDep',
+  components: { HeroBar, CardComponent, TitleBar },
+  data () {
+    return {
+      isLoading: false,
+      form: {
+        name: null,
+        description: null
+      }
+    }
+  },
+  computed: {
+    titleStack () {
+      return [
+        'Insertion',
+        'Departement'
+      ]
+    }
+  },
+  methods: {
+    submit () {
+
+    }
+  }
+}
+</script>
---
+++ src/views/insertion/NewForm.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <title-bar :title-stack="titleStack"/>
+    <hero-bar>
+      Forms
+      <router-link slot="right" to="/" class="button">
+        Dashboard
+      </router-link>
+    </hero-bar>
+    <section class="section is-main-section">
+      <card-component title="Nouvelle formation" icon="ballot">
+        <form @submit.prevent="submit">
+          <b-field label="Départment" horizontal>
+            <b-select placeholder="Selectionne un départment" v-model="form.department" required>
+              <option v-for="(department, index) in departments" :key="index" :value="department">
+                {{ department }}
+              </option>
+            </b-select>
+          </b-field>
+          <b-field label="Titre" message="Titre de la formation" horizontal>
+            <b-input placeholder="e.g. ingenieur des systèmes informatiques" v-model="form.titre" required />
+          </b-field>
+          <b-field label="Description" message="Ne pas dépasser 255 caractères" horizontal>
+            <b-input type="textarea" placeholder="Une petite définition de la formation" v-model="form.description" maxlength="255" required/>
+          </b-field>
+          <b-field horizontal>
+            <b-field grouped>
+              <div class="control">
+                <b-button native-type="submit" type="is-primary">Submit</b-button>
+              </div>
+            </b-field>
+          </b-field>
+        </form>
+      </card-component>
+    </section>
+  </div>
+</template>
+
+<script>
+import TitleBar from '@/components/TitleBar'
+import CardComponent from '@/components/CardComponent'
+import HeroBar from '@/components/HeroBar'
+
+export default {
+  name: 'newForm',
+  components: { HeroBar, CardComponent, TitleBar },
+  data () {
+    return {
+      isLoading: false,
+      form: {
+        department: null,
+        titre: null,
+        description: null
+      },
+      departments: [
+        'Informatique',
+        'Economie',
+        'Electronique'
+      ]
+    }
+  },
+  computed: {
+    titleStack () {
+      return [
+        'Insertion',
+        'Formation'
+      ]
+    }
+  },
+  methods: {
+    submit () {
+
+    }
+  }
+}
+</script>
