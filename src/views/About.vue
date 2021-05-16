cd047e9171f4f9dd24aed8655783fda85a20998d
Abdellatif Beldjelti <a.beldjelti@esi-sba.dz>
2021-05-16 21:39:59 +0100

    Edit logo about

--- src/views/About.vue
+++ src/views/About.vue
@@ -12,7 +12,7 @@
                 <article class="media">
                     <div class="media-left">
                         <figure class="image is-64x64">
-                            <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+                            <img src="img/LogoKUMACF2.png" alt="Image">
                         </figure>
                     </div>
                     <div class="media-content">
