7c4df9e6030e05c5f385ebe3d0d656e370f019b7
Abdellatif Beldjelti <a.beldjelti@esi-sba.dz>
2021-06-15 23:26:09 +0100

    add "show"&"create" club

---
+++ src/views/affichage/Club.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <title-bar :title-stack="titleStack"/>
+    <hero-bar>
+      Table des clubs scientifiques et culturels
+      <router-link slot="right" to="/insertion/club" class="button">
+        Nouveau club
+      </router-link>
+    </hero-bar>
+    <section class="section is-main-section">
+      <card-component class="has-table has-mobile-sort-spaced" title="Clubs" icon="account-multiple">
+        <club-table :data-url="`http://localhost:8080/api/data/clubs`" :checkable="true"/>
+      </card-component>
+    </section>
+  </div>
+
+</template>
+
+<script>
+import CardComponent from '@/components/CardComponent'
+import TitleBar from '@/components/TitleBar'
+import HeroBar from '@/components/HeroBar'
+import ClubTable from '@/components/Tables/ClubTable.vue'
+// import axios from 'axios'
+
+export default {
+  name: 'club',
+  components: { HeroBar, TitleBar, CardComponent, ClubTable },
+  computed: {
+    titleStack () {
+      return [
+        'Tables',
+        'Clubs'
+      ]
+    }
+  }
+/*  created () {
+    console.log(this.$session.get('jwt'))
+    axios.get('http://localhost:8080/api/data/departements', { headers: { 'x-access-token': this.$session.get('jwt') } })
+      .then((response) => {
+        this.listings = response.data
+        console.log(this.listings.results)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  } */
+}
+</script>
---
+++ src/views/insertion/NewClub.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <title-bar :title-stack="titleStack"/>
+    <hero-bar>
+      Insertion
+      <router-link slot="right" to="/tables/clubs" class="button">
+        Table des clubs
+      </router-link>
+    </hero-bar>
+    <section class="section is-main-section">
+      <import-component/>
+      <card-component title="Nouveau club" icon="ballot">
+        <form @submit.prevent="submit">
+          <b-field label="Nom du club" horizontal>
+            <b-field>
+              <b-input icon="account" v-model="form.name" placeholder="Nom du club" name="name" required />
+            </b-field>
+          </b-field>
+          <b-field label="Description" message="Ne pas dépasser 255 caractères" horizontal>
+            <b-input type="textarea" placeholder="Une petite définition du club" v-model="form.description" maxlength="255" required/>
+          </b-field>
+          <b-field label="Type" horizontal>
+                <b-select placeholder="Selectionne un type" v-model="form.type" required>
+                    <option v-for="(type, index) in types" :key="index" :value="type">
+                        {{ type }}
+                    </option>
+                </b-select>
+            </b-field>
+          <b-field horizontal>
+            <b-field grouped>
+              <div class="control">
+                <b-button native-type="submit" type="is-primary">Ajouter</b-button>
+              </div>
+            </b-field>
+          </b-field>
+        </form>
+      </card-component>
+    </section>
+  </div>
+</template>
+
+<script>
+import TitleBar from '@/components/TitleBar'
+import CardComponent from '@/components/CardComponent'
+import HeroBar from '@/components/HeroBar'
+import ImportComponent from '@/components/ImportComponent'
+import axios from 'axios'
+
+export default {
+  name: 'newClub',
+  components: { HeroBar, CardComponent, TitleBar, ImportComponent },
+  data () {
+    return {
+      isLoading: false,
+      form: {
+        name: null,
+        description: null
+      },
+      types: [
+        'Scientifique',
+        'Culturel',
+        'Scietifique et culturel'
+      ]
+    }
+  },
+  computed: {
+    titleStack () {
+      return [
+        'Insertion',
+        'Club'
+      ]
+    }
+  },
+  methods: {
+    submit () {
+      this.isLoading = true
+      axios.post('http://localhost:8080/api/data/departements', {
+        nom: this.form.name,
+        description: this.form.description
+      }, { headers: { 'x-access-token': this.$session.get('jwt') } })
+        .then(response => {
+          this.$buefy.snackbar.open({
+            message: 'le département ' + this.form.name + ' ajouté',
+            queue: false
+          })
+        })
+        .catch(e => {
+          this.errorMessage = e.message
+          console.log('There was an error!', e)
+          this.$buefy.snackbar.open({
+            type: 'is-warning',
+            message: "Erreur d'insertion",
+            queue: false
+          })
+        })
+      setTimeout(() => {
+        this.isLoading = false
+      }, 500)
+    }
+  }
+}
+</script>
