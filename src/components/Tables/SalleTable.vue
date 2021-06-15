dd1348b638e6db0d5f45ab7f1c06c1b8e7ca7573
Abdellatif Beldjelti <a.beldjelti@esi-sba.dz>
2021-06-15 23:27:39 +0100

    add "show"&"create" salle

---
+++ src/components/Tables/SalleTable.vue
@@ -0,0 +1,172 @@
+<template>
+  <div>
+    <modal-box :is-active="isModalActive" :trash-object-name="trashObjectName" @confirm="trashConfirm"
+               @cancel="trashCancel"/>
+    <b-table
+      :checked-rows.sync="checkedRows"
+      :checkable="checkable"
+      :loading="isLoading"
+      :paginated="paginated"
+      :per-page="perPage"
+      :striped="true"
+      :hoverable="true"
+      default-sort="nom"
+      :data="salles">
+
+      <b-table-column label="Nom" field="nom" sortable v-slot="props">
+        {{ props.row.nom }}
+      </b-table-column>
+      <b-table-column label="Type" field="type" sortable v-slot="props">
+        {{ props.row.type }}
+      </b-table-column>
+      <b-table-column label="Created" v-slot="props">
+        <small class="has-text-grey is-abbr-like" :title="props.row.created">{{ props.row.createdAt }}</small>
+      </b-table-column>
+      <b-table-column custom-key="actions" cell-class="is-actions-cell" v-slot="props">
+        <div class="buttons is-right">
+          <router-link :to="{name:'dep.edit', params: {id: props.row.clubId}}" class="button is-small is-primary">
+            <b-icon icon="account-edit" size="is-small"/>
+          </router-link>
+          <button class="button is-small is-danger" type="button" @click.prevent="trashModal(props.row)">
+            <b-icon icon="trash-can" size="is-small"/>
+          </button>
+        </div>
+      </b-table-column>
+
+      <section class="section" slot="empty">
+        <div class="content has-text-grey has-text-centered">
+          <template v-if="isLoading">
+            <p>
+              <b-icon icon="dots-horizontal" size="is-large"/>
+            </p>
+            <p>Fetching data...</p>
+          </template>
+          <template v-else>
+            <p>
+              <b-icon icon="emoticon-sad" size="is-large"/>
+            </p>
+            <p>Nothing's here&hellip;</p>
+          </template>
+        </div>
+      </section>
+    </b-table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import ModalBox from '@/components/ModalBox'
+
+export default {
+  name: 'SalleTable',
+  components: { ModalBox },
+  props: {
+    dataUrl: {
+      default: null
+    },
+    checkable: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      isModalActive: false,
+      trashObject: null,
+      salles: [],
+      isLoading: false,
+      paginated: false,
+      perPage: 10,
+      checkedRows: []
+    }
+  },
+  computed: {
+    trashObjectName () {
+      if (this.trashObject) {
+        return this.trashObject.nom
+      }
+
+      return null
+    }
+  },
+  mounted () {
+    console.log(this.props)
+    console.log(this.$session.get('deps'))
+    /* if (this.dataUrl) {
+      if (r.data && r.data.data) {
+        if (r.data.data.length > this.perPage) {
+          this.paginated = true
+        }
+        this.departements = this.$session.get('deps')
+      }
+    } */
+    if (this.dataUrl) {
+      this.isLoading = true
+      axios
+        .get(this.dataUrl, { headers: { 'x-access-token': this.$session.get('jwt') } })
+        .then(r => {
+          this.isLoading = false
+          if (r.data && r.data.results) {
+            if (r.data.results.length > this.perPage) {
+              this.paginated = true
+            }
+            this.salles = r.data.results
+          }
+        })
+        .catch(e => {
+          this.isLoading = false
+          this.$buefy.toast.open({
+            message: `Error: ${e.message}`,
+            type: 'is-danger'
+          })
+        })
+    }
+  },
+  methods: {
+    trashModal (trashObject) {
+      this.trashObject = trashObject
+      this.isModalActive = true
+    },
+    trashConfirm () {
+      this.isModalActive = false
+      axios.delete('http://localhost:8080/api/data/salles/' + this.trashObject.salleId, { headers: { 'x-access-token': this.$session.get('jwt') } })
+        .then(r => {
+          this.isLoading = false
+          this.$buefy.toast.open({
+            message: 'Confirmed',
+            type: 'is-success'
+          })
+          axios
+            .get(this.dataUrl, { headers: { 'x-access-token': this.$session.get('jwt') } })
+            .then(r => {
+              this.isLoading = false
+              if (r.data && r.data.results) {
+                if (r.data.results.length > this.perPage) {
+                  this.paginated = true
+                }
+                this.salles = r.data.results
+              }
+            })
+            .catch(e => {
+              this.isLoading = false
+              this.$buefy.toast.open({
+                message: `Error: ${e.message}`,
+                type: 'is-danger'
+              })
+            })
+        })
+        .catch(e => {
+          this.isLoading = false
+          console.log(e)
+          this.$buefy.toast.open({
+            message: `Error: ${e.message}`,
+            type: 'is-danger'
+          })
+        })
+    },
+    trashCancel () {
+      this.isModalActive = false
+    }
+  }
+}
+</script>
---
+++ src/views/affichage/Salle.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <title-bar :title-stack="titleStack"/>
+    <hero-bar>
+      Table des salles
+      <router-link slot="right" to="/insertion/salle" class="button">
+        Nouvelle salle
+      </router-link>
+    </hero-bar>
+    <section class="section is-main-section">
+      <card-component class="has-table has-mobile-sort-spaced" title="Salles" icon="account-multiple">
+        <salle-table :data-url="`http://localhost:8080/api/data/salles`" :checkable="true"/>
+      </card-component>
+    </section>
+  </div>
+
+</template>
+
+<script>
+import CardComponent from '@/components/CardComponent'
+import TitleBar from '@/components/TitleBar'
+import HeroBar from '@/components/HeroBar'
+import SalleTable from '@/components/Tables/SalleTable.vue'
+// import axios from 'axios'
+
+export default {
+  name: 'salle',
+  components: { HeroBar, TitleBar, CardComponent, SalleTable },
+  computed: {
+    titleStack () {
+      return [
+        'Tables',
+        'Salles'
+      ]
+    }
+  }
+/*  created () {
+    console.log(this.$session.get('jwt'))
+    axios.get('http://localhost:8080/api/data/departements', { headers: { 'x-access-token': this.$session.get('jwt') } })
+      .then((response) => {
+        this.listings = response.data
+        console.log(this.listings.results)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  } */
+}
+</script>
