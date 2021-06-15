e3945e092c6ebe36b4e3d2007f302b4ca469b04b
Abdellatif Beldjelti <a.beldjelti@esi-sba.dz>
2021-06-15 23:28:35 +0100

    add "show" administratif

--- src/views/affichage/Administratif.vue
+++ src/views/affichage/Administratif.vue
@@ -3,13 +3,13 @@
     <title-bar :title-stack="titleStack"/>
     <hero-bar>
       Table des administratifs
-      <router-link slot="right" to="/insertion/administratif" class="button">
+      <router-link slot="right" to="/insertion/administratifs" class="button">
         Nouveau administratif
       </router-link>
     </hero-bar>
     <section class="section is-main-section">
-      <card-component class="has-table has-mobile-sort-spaced" title="Administratif" icon="account-multiple">
-        <dep-table :data-url="`http://localhost:8080/api/data/administratifs`" :checkable="true"/>
+      <card-component class="has-table has-mobile-sort-spaced" title="Administratifs" icon="account-multiple">
+        <administratif-table :data-url="`http://localhost:8080/api/data/administratifs`" :checkable="true"/>
       </card-component>
     </section>
   </div>
@@ -20,12 +20,12 @@
 import CardComponent from '@/components/CardComponent'
 import TitleBar from '@/components/TitleBar'
 import HeroBar from '@/components/HeroBar'
-import DepTable from '@/components/Tables/DepTable.vue'
+import AdministratifTable from '@/components/Tables/AdministratifTable.vue'
 // import axios from 'axios'
 
 export default {
   name: 'administratif',
-  components: { HeroBar, TitleBar, CardComponent, DepTable },
+  components: { HeroBar, TitleBar, CardComponent, AdministratifTable },
   computed: {
     titleStack () {
       return [
